ad889be15a374ff07492b549ba454bafd3a76fd7
Swen Kooij <swen@sectorlabs.ro>
2018-01-29 12:03:58 +0200

    Cut out logo in README uploaded to PyPi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as readme:
-    README = readme.read()
+    README = readme.read().split('h1>', 2)[1]
 
 setup(
     name='django-postgres-extra',
