7e979d97c8e5b2cece2aecfe5ee72bb7bacb6e11
Swen Kooij <swen@sectorlabs.ro>
2019-01-11 16:59:03 +0200

    Bump version number to 1.21a16

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8
 
 setup(
     name='django-postgres-extra',
-    version='1.21a15',
+    version='1.21a16',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
