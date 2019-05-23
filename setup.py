1cb027fb0dce5d0d29993d46d35a9b94cbe1482a
Swen Kooij <swen@sectorlabs.ro>
2019-05-23 11:05:39 +0300

    Let's do another alpha release to test with 1.21a17

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open(
 
 setup(
     name="django-postgres-extra",
-    version="1.21",
+    version="1.21a17",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
