b5e8881d33e85460d95e70cb8285c26c9a1f345b
Swen Kooij <swen@sectorlabs.ro>
2019-09-15 12:51:54 +0300

    Short import in pgmakemigrations

--- psqlextra/management/commands/pgmakemigrations.py
+++ psqlextra/management/commands/pgmakemigrations.py
@@ -1,10 +1,9 @@
-from django.core.management.commands.makemigrations import \
-    Command as MakeMigrationsCommand
+from django.core.management.commands import makemigrations
 
 from psqlextra.backend.migrations import postgres_migrations
 
 
-class Command(MakeMigrationsCommand):
+class Command(makemigrations.Command):
     help = "Creates new PostgreSQL specific migration(s) for apps."
 
     def handle(self, *app_labels, **options):
