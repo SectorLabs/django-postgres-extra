1f6eea9a6793abc76db36eff4b194ce994be457a
Swen Kooij <swen@sectorlabs.ro>
2019-09-16 15:46:00 +0300

    Expand auto partitioning tests

--- tests/test_auto_partition.py
+++ tests/test_auto_partition.py
@@ -10,8 +10,10 @@ from psqlextra.auto_partition import (
 from .fake_model import define_fake_partitioning_model
 
 
-@freezegun.freeze_time("1337-01-02")
 def test_auto_partition():
+    """Tests whether automatically creating new partitions ahead works as
+    expected."""
+
     model = define_fake_partitioning_model(
         {"timestamp": models.DateTimeField()}, {"key": ["timestamp"]}
     )
@@ -20,12 +22,13 @@ def test_auto_partition():
     schema_editor.create_partitioned_model(model)
 
     # create partitions for the next 12 months (including the current)
-    postgres_auto_partition(
-        model,
-        count=12,
-        interval_unit=PostgresAutoPartitioningIntervalUnit.MONTH,
-        interval=1,
-    )
+    with freezegun.freeze_time("1337-01-30"):
+        postgres_auto_partition(
+            model,
+            count=12,
+            interval_unit=PostgresAutoPartitioningIntervalUnit.MONTH,
+            interval=1,
+        )
 
     with connection.cursor() as cursor:
         table = connection.introspection.get_partitioned_table(
@@ -47,12 +50,13 @@ def test_auto_partition():
     assert table.partitions[11].name == f"{model._meta.db_table}_1337_dec"
 
     # re-running it with 13, should just create one additional partition
-    postgres_auto_partition(
-        model,
-        count=13,
-        interval_unit=PostgresAutoPartitioningIntervalUnit.MONTH,
-        interval=1,
-    )
+    with freezegun.freeze_time("1337-01-30"):
+        postgres_auto_partition(
+            model,
+            count=13,
+            interval_unit=PostgresAutoPartitioningIntervalUnit.MONTH,
+            interval=1,
+        )
 
     with connection.cursor() as cursor:
         table = connection.introspection.get_partitioned_table(
@@ -61,3 +65,21 @@ def test_auto_partition():
 
     assert len(table.partitions) == 13
     assert table.partitions[12].name == f"{model._meta.db_table}_1338_jan"
+
+    # it's november now, we only want to create 4 partitions ahead,
+    # so only one new partition should be created for february 1338
+    with freezegun.freeze_time("1337-11-01"):
+        postgres_auto_partition(
+            model,
+            count=4,
+            interval_unit=PostgresAutoPartitioningIntervalUnit.MONTH,
+            interval=1,
+        )
+
+    with connection.cursor() as cursor:
+        table = connection.introspection.get_partitioned_table(
+            cursor, model._meta.db_table
+        )
+
+    assert len(table.partitions) == 14
+    assert table.partitions[13].name == f"{model._meta.db_table}_1338_feb"
