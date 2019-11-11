3eba7ca21e91852c58f39e59d29846d4dc6f9e38
Swen Kooij <swen@sectorlabs.ro>
2019-11-11 10:18:18 +0200

    Export all partitioning related classes

--- psqlextra/partitioning/__init__.py
+++ psqlextra/partitioning/__init__.py
@@ -1,9 +1,23 @@
 from .error import PostgresPartitioningError
 from .manager import PostgresPartitioningManager
+from .partition import PostgresPartition
+from .plan import PostgresModelPartitioningPlan, PostgresPartitioningPlan
+from .range_partition import PostgresRangePartition
 from .shorthands import partition_by_time
+from .strategy import PostgresPartitioningStrategy
+from .time_partition import PostgresTimePartition
+from .time_strategy import PostgresTimePartitioningStrategy
 
 __all__ = [
     "PostgresPartitioningManager",
     "partition_by_time",
     "PostgresPartitioningError",
+    "PostgresPartitioningPlan",
+    "PostgresModelPartitioningPlan",
+    "PostgresPartition",
+    "PostgresRangePartition",
+    "PostgresTimePartition",
+    "PostgresPartitioningStrategy",
+    "PostgresTimePartitioningStrategy",
+    "PostgresRangePartitioningStrategy" "PostgresPartitioningConfig",
 ]
