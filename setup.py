cc21241751d4aa913886f0f9462358eac95897ef
Swen Kooij <swen@sectorlabs.ro>
2020-10-19 18:15:23 +0300

    Bump version to 2.0.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open(
 
 setup(
     name="django-postgres-extra",
-    version="2.0.1",
+    version="2.0.2",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
