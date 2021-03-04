938f482d99a03f6978043c9ff878f5f63f3f9967
Swen Kooij <swen@sectorlabs.ro>
2021-03-04 18:38:59 +0200

    Bump version number to 2.0.3rc3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open(
 
 setup(
     name="django-postgres-extra",
-    version="2.0.3rc2",
+    version="2.0.3rc3",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
