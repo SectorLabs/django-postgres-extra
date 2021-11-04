21d880ffadc0462e5afce467b962d73fda858c7d
Swen Kooij <swen@sectorlabs.ro>
2021-11-04 14:33:38 +0200

    Update test dependencies for compatibility with Python 3.10

--- tests/snapshots/snap_test_management_command_partition.py
+++ tests/snapshots/snap_test_management_command_partition.py
@@ -2,160 +2,33 @@
 # snapshottest: v1 - https://goo.gl/zC4yUc
 from __future__ import unicode_literals
 
-from snapshottest import Snapshot
+from snapshottest import GenericRepr, Snapshot
+
 
 snapshots = Snapshot()
 
-snapshots["test_management_command_partition_dry_run[-d] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
+snapshots['test_management_command_partition_auto_confirm[--yes] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nOperations applied.\\n', err='')")
 
-1 partitions will be deleted
-1 partitions will be created
-""",
-    "",
-)
+snapshots['test_management_command_partition_auto_confirm[-y] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nOperations applied.\\n', err='')")
 
-snapshots["test_management_command_partition_dry_run[--dry] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
+snapshots['test_management_command_partition_confirm_no[NO] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nDo you want to proceed? (y/N) Operation aborted.\\n', err='')")
 
-1 partitions will be deleted
-1 partitions will be created
-""",
-    "",
-)
+snapshots['test_management_command_partition_confirm_no[N] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nDo you want to proceed? (y/N) Operation aborted.\\n', err='')")
 
-snapshots["test_management_command_partition_auto_confirm[-y] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
+snapshots['test_management_command_partition_confirm_no[No] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nDo you want to proceed? (y/N) Operation aborted.\\n', err='')")
 
-1 partitions will be deleted
-1 partitions will be created
-Operations applied.
-""",
-    "",
-)
+snapshots['test_management_command_partition_confirm_no[n] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nDo you want to proceed? (y/N) Operation aborted.\\n', err='')")
 
-snapshots["test_management_command_partition_auto_confirm[--yes] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
+snapshots['test_management_command_partition_confirm_no[no] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nDo you want to proceed? (y/N) Operation aborted.\\n', err='')")
 
-1 partitions will be deleted
-1 partitions will be created
-Operations applied.
-""",
-    "",
-)
+snapshots['test_management_command_partition_confirm_yes[YES] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nDo you want to proceed? (y/N) Operations applied.\\n', err='')")
 
-snapshots["test_management_command_partition_confirm_yes[y] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
+snapshots['test_management_command_partition_confirm_yes[Y] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nDo you want to proceed? (y/N) Operations applied.\\n', err='')")
 
-1 partitions will be deleted
-1 partitions will be created
-Do you want to proceed? (y/N) Operations applied.
-""",
-    "",
-)
+snapshots['test_management_command_partition_confirm_yes[y] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nDo you want to proceed? (y/N) Operations applied.\\n', err='')")
 
-snapshots["test_management_command_partition_confirm_yes[Y] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
+snapshots['test_management_command_partition_confirm_yes[yes] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\nDo you want to proceed? (y/N) Operations applied.\\n', err='')")
 
-1 partitions will be deleted
-1 partitions will be created
-Do you want to proceed? (y/N) Operations applied.
-""",
-    "",
-)
+snapshots['test_management_command_partition_dry_run[--dry] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\n', err='')")
 
-snapshots["test_management_command_partition_confirm_yes[yes] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
-
-1 partitions will be deleted
-1 partitions will be created
-Do you want to proceed? (y/N) Operations applied.
-""",
-    "",
-)
-
-snapshots["test_management_command_partition_confirm_yes[YES] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
-
-1 partitions will be deleted
-1 partitions will be created
-Do you want to proceed? (y/N) Operations applied.
-""",
-    "",
-)
-
-snapshots["test_management_command_partition_confirm_no[n] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
-
-1 partitions will be deleted
-1 partitions will be created
-Do you want to proceed? (y/N) Operation aborted.
-""",
-    "",
-)
-
-snapshots["test_management_command_partition_confirm_no[N] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
-
-1 partitions will be deleted
-1 partitions will be created
-Do you want to proceed? (y/N) Operation aborted.
-""",
-    "",
-)
-
-snapshots["test_management_command_partition_confirm_no[no] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
-
-1 partitions will be deleted
-1 partitions will be created
-Do you want to proceed? (y/N) Operation aborted.
-""",
-    "",
-)
-
-snapshots["test_management_command_partition_confirm_no[No] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
-
-1 partitions will be deleted
-1 partitions will be created
-Do you want to proceed? (y/N) Operation aborted.
-""",
-    "",
-)
-
-snapshots["test_management_command_partition_confirm_no[NO] 1"] = (
-    """test:
-  - tobedeleted
-  + tobecreated
-
-1 partitions will be deleted
-1 partitions will be created
-Do you want to proceed? (y/N) Operation aborted.
-""",
-    "",
-)
+snapshots['test_management_command_partition_dry_run[-d] 1'] = GenericRepr("CaptureResult(out='test:\\n  - tobedeleted\\n  + tobecreated\\n\\n1 partitions will be deleted\\n1 partitions will be created\\n', err='')")
