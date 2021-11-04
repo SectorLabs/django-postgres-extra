a70aba36fa308c30b6b0004b867f2d54dc306b3b
Swen Kooij <swen@sectorlabs.ro>
2021-11-04 15:13:07 +0200

    Fix #149: PostgresRangePartitioningStrategy should derive from PostgresPartitioningStrategy

--- psqlextra/partitioning/range_strategy.py
+++ psqlextra/partitioning/range_strategy.py
@@ -1,24 +1,9 @@
-from abc import abstractmethod
-from typing import Generator
-
-from .range_partition import PostgresRangePartition
+from .strategy import PostgresPartitioningStrategy
 
 
-class PostgresRangePartitioningStrategy:
+class PostgresRangePartitioningStrategy(PostgresPartitioningStrategy):
     """Base class for implementing a partitioning strategy for a range
     partitioned table."""
-
-    @abstractmethod
-    def to_create(
-        self,
-    ) -> Generator[PostgresRangePartition, None, None]:
-        """Generates a list of partitions to be created."""
-
-    @abstractmethod
-    def to_delete(
-        self,
-    ) -> Generator[PostgresRangePartition, None, None]:
-        """Generates a list of partitions to be deleted."""
 
 
 __all__ = ["PostgresRangePartitioningStrategy"]
