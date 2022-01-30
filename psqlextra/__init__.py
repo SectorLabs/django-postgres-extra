a00751fd330cf7327bc5de95aca1994dbb86c92e
Swen Kooij <swen@sectorlabs.ro>
2022-01-30 15:56:53 +0200

    Let CircleCI publish the package to PyPi

--- psqlextra/__init__.py
+++ psqlextra/__init__.py
@@ -1,4 +1,15 @@
 import django
 
+from ._version import __version__
+
 if django.VERSION < (3, 2):  # pragma: no cover
     default_app_config = "psqlextra.apps.PostgresExtraAppConfig"
+
+    __all__ = [
+        "default_app_config",
+        "__version__",
+    ]
+else:
+    __all__ = [
+        "__version__",
+    ]
