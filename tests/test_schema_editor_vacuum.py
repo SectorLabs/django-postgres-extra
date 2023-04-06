4bf20e694232822120e3aa767abe18b12082c81f
Swen Kooij <swen@sectorlabs.ro>
2023-04-06 14:32:06 +0300

    Add `vacuum` method to schema editor

---
+++ tests/test_schema_editor_vacuum.py
@@ -0,0 +1,80 @@
+import pytest
+
+from django.core.exceptions import (
+    SuspiciousOperation,
+)
+from django.db import connection, models
+from django.test.utils import CaptureQueriesContext
+
+from psqlextra.backend.schema import PostgresSchemaEditor
+
+from .fake_model import delete_fake_model, get_fake_model
+
+
+@pytest.fixture
+def fake_model():
+    model = get_fake_model(
+        {
+            "name": models.TextField(),
+        }
+    )
+
+    yield model
+
+    delete_fake_model(model)
+
+
+def test_schema_editor_vacuum_not_in_transaction(fake_model):
+    schema_editor = PostgresSchemaEditor(connection)
+
+    with pytest.raises(SuspiciousOperation):
+        schema_editor.vacuum(fake_model._meta.db_table)
+
+
+@pytest.mark.parametrize(
+    "kwargs,query",
+    [
+        (dict(), "VACUUM %s"),
+        (dict(full=True), "VACUUM (FULL) %s"),
+        (dict(analyze=True), "VACUUM (ANALYZE) %s"),
+        (dict(parallel=8), "VACUUM (PARALLEL 8) %s"),
+        (dict(analyze=True, verbose=True), "VACUUM (VERBOSE, ANALYZE) %s"),
+        (
+            dict(analyze=True, parallel=8, verbose=True),
+            "VACUUM (VERBOSE, ANALYZE, PARALLEL 8) %s",
+        ),
+        (dict(freeze=True), "VACUUM (FREEZE) %s"),
+        (dict(verbose=True), "VACUUM (VERBOSE) %s"),
+        (dict(disable_page_skipping=True), "VACUUM (DISABLE_PAGE_SKIPPING) %s"),
+        (dict(skip_locked=True), "VACUUM (SKIP_LOCKED) %s"),
+        (dict(index_cleanup=True), "VACUUM (INDEX_CLEANUP) %s"),
+        (dict(truncate=True), "VACUUM (TRUNCATE) %s"),
+    ],
+)
+@pytest.mark.django_db(transaction=True)
+def test_schema_editor_vacuum(fake_model, kwargs, query):
+    schema_editor = PostgresSchemaEditor(connection)
+
+    with CaptureQueriesContext(connection) as ctx:
+        schema_editor.vacuum(fake_model._meta.db_table, **kwargs)
+
+    queries = [query["sql"] for query in ctx.captured_queries]
+    assert queries == [
+        query % connection.ops.quote_name(fake_model._meta.db_table)
+    ]
+
+
+@pytest.mark.django_db(transaction=True)
+def test_schema_editor_vacuum_columns(fake_model):
+    schema_editor = PostgresSchemaEditor(connection)
+
+    with CaptureQueriesContext(connection) as ctx:
+        schema_editor.vacuum(
+            fake_model._meta.db_table, ["id", "name"], analyze=True
+        )
+
+    queries = [query["sql"] for query in ctx.captured_queries]
+    assert queries == [
+        'VACUUM (ANALYZE) %s ("id", "name")'
+        % connection.ops.quote_name(fake_model._meta.db_table)
+    ]
