b5e04d04b1fe784302de0cbaaa63f7f2be4ec96b
loicgasser <loicgasser4@gmail.com>
2023-05-31 23:18:27 -0400

    chore(): Add missing python versions in setup.py

--- setup.py
+++ setup.py
@@ -61,6 +61,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
